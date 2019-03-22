2537de379b993dfa7c7401127b04c0e51b6d8084
Taras Kopanskyi <tkopanskyi@gmail.com>
2019-03-22 15:49:28 +0200

    Add project files.

---
+++ Workers_TestTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Workers_TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
